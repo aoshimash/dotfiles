ddaff2d0e002a140c794b8fd26a97563b3474970
Shuji Aoshima <47586723+aoshimash@users.noreply.github.com>
2024-08-26 14:04:27 +0900

    path更新

--- dot_zsh.d/path.zsh
+++ dot_zsh.d/path.zsh
@@ -11,4 +11,6 @@ path=(
   $HOME/.pub-cache/bin
   $(brew --prefix)/share/google-cloud-sdk/path.zsh.inc # for gcloud
   $(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc # for gcloud
+  $(brew --prefix)/opt/openjdk/bin
+  $(brew --prefix)/opt/dart@2.12/bin
 )
