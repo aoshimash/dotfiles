07dd748438c96c674b4017e1d688e2e152d6a544
Shuji Aoshima <47586723+aoshimash@users.noreply.github.com>
2024-10-28 01:18:04 +0900

    fzfの初期化を追加

--- dot_zsh.d/fzf.zsh
+++ dot_zsh.d/fzf.zsh
@@ -1,3 +1,5 @@
+source <(fzf --zsh)
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 #export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 #export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
