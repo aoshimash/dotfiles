84c192a15560378bdfe54dea1ef762e91d506285
Shuji Aoshima <shuji.aoshima@nulab.com>
2025-07-08 12:20:23 +0900

    環境変数追加

--- dot_zsh.d/env.zsh
+++ dot_zsh.d/env.zsh
@@ -3,3 +3,10 @@ export VISUAL="emacs"
 export GOPATH="$HOME/go"
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 export NPM_CONFIG_PREFIX=${XDG_DATA_HOME:-$HOME/.local/share}/npm-global # for aqua https://aquaproj.github.io/docs/reference/nodejs-support/
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.google/service-account-key.json"
+
+if [[ "$(hostname)" == "JPN-2025-020" ]]; then
+  export GOOGLE_CLOUD_PROJECT="nulab-developer-platform"
+  export GOOGLE_CLOUD_LOCATION="global"
+  export GOOGLE_GENAI_USE_VERTEXAI=true
+fi
