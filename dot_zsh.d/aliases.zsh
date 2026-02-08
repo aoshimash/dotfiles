11e5172ef3ed0ac3682359aed1389f81a767ff2c
Shuji Aoshima <47586723+aoshimash@users.noreply.github.com>
2026-02-09 01:26:33 +0900

    Refactor claude alias for JPN-2025-020 to include BedrockUsers profile and update path

--- dot_zsh.d/aliases.zsh
+++ dot_zsh.d/aliases.zsh
@@ -3,7 +3,6 @@ alias k='kubectl'
 alias ksec='kubesec'
 alias wk='watch kubectl'
 alias ls='ls -G'
-alias claude='/Users/aoshima/.local/share/npm-global/bin/claude'
 
 if [[ "$(hostname)" == "JPN-2025-020" ]]; then
   alias gemini='/Users/aoshima/.local/share/npm-global/bin/gemini'
